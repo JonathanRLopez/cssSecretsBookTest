471424d6f6f2baca27b394cb3d29e9a8dde2a4b1
jonathan rojas <jonathan@monoku.com>
2017-09-22 18:01:22 -0500

    patterns componets bg

--- src/components/exampleFive/exampleFive.js
+++ src/components/exampleFive/exampleFive.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import glamorous, {H2} from 'glamorous'
 
-const SubContainer = glamorous.div(
-  {
+const designOfBackgrounds = {
+  horizontal: {
     background: 'linear-gradient(#fb3 33.3%, #58a 0, #58a 66.6%, yellowgreen 0)',
     backgroundSize: '100% 45px',
+  },
+  vertical: {
+    background: 'linear-gradient(to right, #fb3 50%, #58a 0)',
+    backgroundSize: '30px 100%',
+  },
+  diagonal: {
+    background: 'repeating-linear-gradient(45deg, #fb3 0, #fb3 25%, #58a 0, #58a 50%)',
+    backgroundSize: '30px 30px'
+  },
+  dinamicDiagonal: {
+    background: '#58a',
+    backgroundImage: `repeating-linear-gradient(30deg,
+                            hsla(0,0%,100%,.1),
+                            hsla(0,0%,100%,.1) 15px,
+                            transparent 0, transparent 30px)`,
+  }
+}
+const SubContainer = glamorous.div(
+  {
     width: '80%',
     height: '75%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     color: 'white',
-  }
+  },
+  props => designOfBackgrounds[props.type] || designOfBackgrounds.horizontal
 )
 
 const Container = glamorous.div({
@@ -31,7 +51,7 @@ class exampleFive extends Component {
           {type} stripes bg
         </H2>
         img
-        <SubContainer />   
+        <SubContainer type={type} />   
       </Container>
     );
   }
